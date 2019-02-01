f1ce9ade72b7e9ccd39f02abf577d8018937aeff
Rafael Pumarejo Murphy <rafaelpmurphy@gmail.com>
2019-02-01 15:18:41 -0500

    Initial Exercise 2 Submission

---
+++ exercises/exercise02/Assets/cubescript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubescript : MonoBehaviour
+
+{
+    float force = 0;
+    Vector3 startPosition;
+    Quaternion startRotation;
+
+    bool playerWon = false;
+
+            void Start()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKey(KeyCode.Space)) {
+            force = force + 3f;
+            }
+       
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * force);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Oh no!!!");       
+    }
+}
---
+++ exercises/exercise02/Assets/photon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class photon : MonoBehaviour
+{
+    bool GoLuke = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            Rigidbody rbp = gameObject.GetComponent<Rigidbody>();
+            rbp.useGravity = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "ExhaustTube")
+        {
+            GoLuke = true;
+        }
+    }
+}
+
